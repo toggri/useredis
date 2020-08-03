7c4fc8ab81324b965eda0d4bb13fcc6ed03664c2
Daniel Shim <toggri@gmail.com>
2020-08-03 15:33:03 +0900

    add query and modify code

--- sample.py
+++ sample.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3.4
-
+# -*- coding:utf-8 -*- 
 import redis
 import pymysql
 import config
 
-r=redis.Redis(host=config.RHOST,port=config.RPORT,db=config.RDB,password=config.RPASSWORD)
-KEY='key'
-VAL='value'
-r.setnx(KEY,VAL)
-rval = r.get(KEY)
-print("Redis Data {} => {} ".format(KEY,rval))
+# connect redis
+try:
+   r=redis.Redis(host=config.RHOST,port=config.RPORT,db=config.RDB,password=config.RPASSWORD)
+   print("Redis Connected {} ".format(r))
+except e:
+   print("Error redis {}".format(e))
 
-db=pymysql.connect(config.MHOST,config.MUSER,config.MPASSWORD,config.MDB)
-cur=db.cursor()
-cur.execute("select version()")
-data = cur.fetchone()
-print("Database version {}".format(data))
-db.close()
+# connect mysql
+try:
+   db=pymysql.connect(config.MHOST,config.MUSER,config.MPASSWORD,config.MDB)
+   cur=db.cursor()
+   print("Mysql Connected {} ".format(cur))
+except e:
+   print("Error mysql {}".format(e))
+
+
+# select data
+try:
+   query=config.QUERY
+   cur.execute(query)
+   data = cur.fetchall()
+   for d in data:
+      print("{}".format(d[0]))
+     # insert into redis db0 
+      r.setex(d[0],86400,"")
+except e:
+   print("Error main process {}".format(e))
+finally:
+   db.close()
+
+# end.
